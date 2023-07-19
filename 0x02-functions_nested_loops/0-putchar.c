73cf1dc7029b2309479eb296ee6ea8b40f9c51ec
islamkhalid295 <islamkhalid295@gmail.com>
2023-07-19 16:59:06 -0700

    00

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-holberton.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
+#include "main.h"
+
+/**
+ * main - Prints "Holberton" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar");
+	_putchar('\n');
+
+	return (0);
+}
