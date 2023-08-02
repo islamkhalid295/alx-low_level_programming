593039b0f1f77b58f2e9a97d00e845d0ce0aec15
islamkhalid295 <islamkhalid295@gmail.com>
2023-08-01 18:35:15 -0700

    finish

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,7 +6,6 @@
 #include "holberton.h"
 
 /**
- * strcat - Concatenates the string pointed to by @src, including the terminating
  *          null byte, to the end of the string pointed to by @dest.
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to @dest.
